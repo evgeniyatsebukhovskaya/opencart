3a843a72cc052e5250f343ffac8ae2f712cdf1d8
Evgeniya <e.tsebukhovskaya@gmail.com>
2019-09-08 16:11:52 +0700

    search_elements_task

---
+++ MainPage.py
@@ -0,0 +1,30 @@
+import pytest
+from time import sleep
+from selenium import webdriver
+
+
+def driver():
+    d = webdriver.Firefox()
+    d.maximize_window()
+    d.get('http://localhost/opencart/')
+    yield d
+    d.quit()
+
+class MainPage():
+
+    header = '#top'
+    currency_button = './/form[@id="form-currency"]//button'
+    menu_section = '#menu'
+    desktops_button = './/a[text()="Desktops"]'
+    about_us_button = './/a[text()="About Us"]'
+    search_field = './/input[@name="search"]'
+    search_button = './/div[@id="search"]//button'
+
+class Test_Page():
+
+    def test_search(self, driver):
+        main_page = MainPage()
+        driver.find_element_by_xpath(main_page.search_field).send_keys("phone")
+        driver.find_element_by_xpath(main_page.search_button).click()
+        assert driver.find_element_by_xpath('.//h1[text()="Search - phone"]').is_displayed()
+
--- conftest.py
+++ conftest.py
@@ -4,13 +4,16 @@ from pathlib import Path
 
 @pytest.fixture
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    path = Path(__file__).resolve().parent.parent.parent.joinpath('chromedriver.exe')
-    d = webdriver.Chrome(str(path), options=options)
-
+    d = webdriver.Firefox()
     d.maximize_window()
-    d.implicitly_wait(10)  # Set the implicitly wait time to 10 seconds
     d.get('http://localhost/opencart/')
     yield d
     d.quit()
+   # options = webdriver.ChromeOptions()
+    #options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    #path = Path(__file__).resolve().parent.parent.parent.joinpath('chromedriver.exe')
+   # path = Path.cwd() / 'chromedriver.exe'
+    #d = webdriver.Chrome(str(path), options=options)
+    #d = webdriver.Chrome(str(path), options=options)
+
+   # d.implicitly_wait(10)  # Set the implicitly wait time to 10 seconds
