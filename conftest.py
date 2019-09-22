ad4f1f5643ce8860b2c2fa06c0023c3a56dc8484
Evgeniya <e.tsebukhovskaya@gmail.com>
2019-09-22 23:22:19 +0700

    [+] waitings

--- conftest.py
+++ conftest.py
@@ -4,11 +4,11 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.ie.options import Options as IeOptions
 
-from pathlib import Path
 
+'''
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="choose chrome, firefox or ie")
-    parser.addoption("--url", action="store", default="http://installopencart.localhost/", help="choose your browser")
+    parser.addoption("--url", action="store", default="http://opencart.eng/", help="choose your browser")
 
 
 @pytest.fixture
@@ -32,4 +32,5 @@ def driver(request):
     yield d
     d.quit()
     return d
+'''
 
